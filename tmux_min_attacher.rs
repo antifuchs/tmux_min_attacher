0c4ebd4cf2ea991ac93a052ae884996c97be5968
Andreas Fuchs <asf@boinkor.net>
2013-09-14 21:47:26 -0700

    Add execvp()ing, and run tmux with the right args!
    This is exciting, as it now means I can use this as my shell (mwahaha).

--- tmux_min_attacher.rs
+++ tmux_min_attacher.rs
@@ -4,6 +4,10 @@ use std::run;
 use std::str;
 use std::option::*;
 use std::trie::*;
+use std::libc::{execvp, perror};
+use std::c_str::*;
+use std::vec;
+use std::ptr;
 
 fn session_is_attached(line: &str) -> bool {
     line.ends_with("(attached)")
@@ -26,6 +30,21 @@ fn detached_sessions(output: ~str) -> TrieSet {
     set
 }
 
+#[fixed_stack_segment]
+fn exec_program(program: &str, args: &[~str]) {
+    do program.to_c_str().with_ref() |c_program| {
+        // I don't much care about the ownership of the strings here
+        // at this point, so let's just fail if execvp isn't working.
+        unsafe {
+            let mut c_args = args.map(|arg| { arg.to_c_str().unwrap() });
+            c_args.push(ptr::null());
+            execvp(c_program, vec::raw::to_ptr(c_args));
+            perror("Running tmux failed:".to_c_str().unwrap());
+        }
+        fail!(fmt!("Couldn't exec %s", program));
+    }
+}
+
 fn main() {
     let proc = run::process_output("tmux", &[~"list-sessions"]);
     if proc.status != 0 {
@@ -34,8 +53,11 @@ fn main() {
     let output = str::from_utf8(proc.output);
     let sessions = detached_sessions(output);
     match sessions.iter().next() {
-        Some(n) => { println(fmt!("Can attach to %u", n)); }
-        _ => { println("Have no sessions to attach to"); }
+        Some(n) => {
+            let session = n.to_str();
+            exec_program("tmux", &[~"tmux", ~"attach-session", ~"-t", session]);
+        }
+        _ => { exec_program("tmux", &[~"tmux"]); }
     }
 }
 
