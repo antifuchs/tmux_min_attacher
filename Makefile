90d5d191a107feeb7d5908fb8d9151b8ef36d7fe
Andreas Fuchs <asf@boinkor.net>
2013-12-05 18:18:54 -0800

    Bump to rust 0.9-pre
    Some tiny changes; some even make the program more readable; who'd
    a-thought (-:

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 tmux_min_attacher: tmux_min_attacher.rs
 	rustc tmux_min_attacher.rs
 
-run:
-	rust run tmux_min_attacher.rs
+run: tmux_min_attacher
+	./tmux_min_attacher
 
-test:
-	rust test tmux_min_attacher.rs
+test: tmux_min_attacher-test
+	./tmux_min_attacher-test
+	
+tmux_min_attacher-test: tmux_min_attacher.rs
+	rustc --test -o tmux_min_attacher-test tmux_min_attacher.rs
 
 clean:
 	git clean -fdx
+	
+install: tmux_min_attacher
+	cp tmux_min_attacher ~/bin
--- tmux_min_attacher.rs
+++ tmux_min_attacher.rs
@@ -2,10 +2,10 @@ extern mod std;
 
 use std::run;
 use std::str;
-use std::option::*;
-use std::trie::*;
+use std::option::Option;
+use std::trie::TrieSet;
 use std::libc::{execvp, perror};
-use std::c_str::*;
+//use std::c_str::ptr;
 use std::vec;
 use std::ptr;
 use std::os;
@@ -14,29 +14,28 @@ fn detached_session_number(line: &str) -> Option<uint> {
     if line.ends_with("(attached)") {
         None
     } else {
-        line.split_iter(':').next().and_then(|s| from_str(s))
+        line.split(':').next().and_then(|s| from_str(s))
     }
 }
 
 fn detached_sessions(output: ~str) -> TrieSet {
-    output.line_iter().filter_map(|line| {
+    output.lines().filter_map(|line| {
             detached_session_number(line)
     }).collect()
 }
 
-#[fixed_stack_segment]
 fn exec_program(program: &str, args: &[~str]) {
-    do program.with_c_str() |c_program| {
-        // I don't much care about the ownership of the strings here
-        // at this point, so let's just fail if execvp isn't working.
-        unsafe {
-            let mut c_args = args.map(|arg| { arg.to_c_str().unwrap() });
-            c_args.push(ptr::null());
-            execvp(c_program, vec::raw::to_ptr(c_args));
-            perror("Running tmux failed:".to_c_str().unwrap());
-        }
-        fail!(fmt!("Couldn't exec %s", program));
-    }
+    program.with_c_str(|c_program| {
+            // I don't much care about the ownership of the strings here
+            // at this point, so let's just fail if execvp isn't working.
+            unsafe {
+                let mut c_args = args.map(|arg| { arg.to_c_str().unwrap() });
+                c_args.push(ptr::null());
+                execvp(c_program, vec::raw::to_ptr(c_args));
+                perror("Running tmux failed:".to_c_str().unwrap());
+            }
+        });
+    fail!(format!("Couldn't exec {:s}", program));
 }
 
 fn prepare_environment() {
@@ -48,9 +47,9 @@ fn prepare_environment() {
 }
 
 fn start_server() {
-    let proc = run::process_output("tmux", &[~"start-server"]);
-    if proc.status != 0 {
-        fail!(fmt!("Could not start tmux server: exited with an error status: %d", proc.status));
+    let program = run::process_output("tmux", &[~"start-server"]);
+    if !program.status.success() {
+        fail!(format!("Could not start tmux server: exited with an error status: {:?}", program.status));
     }
 }
 
@@ -58,11 +57,11 @@ fn main() {
     prepare_environment();
 
     start_server();
-    let proc = run::process_output("tmux", &[~"list-sessions"]);
-    if proc.status != 0 {
-        fail!(fmt!("Tmux exited with an error status: %d", proc.status));
+    let program = run::process_output("tmux", &[~"list-sessions"]);
+    if !program.status.success() {
+        fail!(format!("Tmux exited with an error status: {:?}", program.status));
     }
-    let output = str::from_utf8(proc.output);
+    let output = str::from_utf8(program.output).to_owned();
     let sessions = detached_sessions(output);
     match sessions.iter().next() {
         Some(n) => {
@@ -77,7 +76,7 @@ fn main() {
 fn test_session_number_with_numbers(){
     match(detached_session_number("11: 1 windows (created Sat Sep 14 17:11:29 2013) [130x65]")) {
         Some(11) => (),
-        Some(n) => fail!(fmt!("Should have returned 11, got %u!", n)),
+        Some(n) => fail!(format!("Should have returned 11, got {:u}!", n)),
         None => fail!("Should have returned something, got nothing"),
     }
 }
@@ -85,7 +84,7 @@ fn test_session_number_with_numbers(){
 #[test]
 fn test_session_number_with_strings(){
     match(detached_session_number("oink: 1 windows (created Sat Sep 14 17:11:29 2013) [130x65]")) {
-        Some(n) => fail!(fmt!("Should have returned None, got %u!", n)),
+        Some(n) => fail!(format!("Should have returned None, got {:u}!", n)),
         None => ()
     }
 }
@@ -93,7 +92,7 @@ fn test_session_number_with_strings(){
 #[test]
 fn test_session_number_with_attached_session(){
     match(detached_session_number("1: 1 windows (created Sat Sep 14 17:11:29 2013) [130x65] (attached)")) {
-        Some(n) => fail!(fmt!("Should have returned None, got %u!", n)),
+        Some(n) => fail!(format!("Should have returned None, got {:u}!", n)),
         None => ()
     }
 }
